864abb139bb09f305c2f38cacc661ba1b90ce442
VuLeDuy1 <152946128+VuLeDuy1@users.noreply.github.com>
2026-08-13 05:26:56 +0700

    changed character sprite name logic and added new sprites

--- src/LineagePage.ts
+++ src/LineagePage.ts
@@ -19,7 +19,7 @@ import type { CharacterOption } from './components/CharacterPickerSingle.svelte'
 export const baseOptions: CharacterOption[] = characters
     .filter((c: any) => c.released === 1)
     .map((c: any) => {
-    const imagePath = `/src/assets/character_sprites/${c.name}.png`;
+    const imagePath = `/src/assets/character_sprites/char${c.id}.png`;
 
     return {
         id: c.id,
